3aa0f051de2368429c3ed33409a11cd9765242fa
Avneesh Mishra <123avneesh@gmail.com>
2017-09-30 22:20:23 +0530

    Third edit

---
+++ Actuator_Hammer_3/Actuator_Hammer_3.ino
@@ -0,0 +1,126 @@
+/*
+   With relays
+*/
+class Ultrasonic_Sensor {
+    int TRIG_PIN;
+    int ECHO_PIN;
+
+    float THRESHOLD_VALUE;
+
+    float DISTANCE;
+  public:
+    void AttachToTrigEcho(int Trigger, int Echo) {
+      TRIG_PIN = Trigger;
+      ECHO_PIN = Echo;
+      pinMode(TRIG_PIN, OUTPUT);
+      pinMode(ECHO_PIN, INPUT);
+    }
+
+    bool Did_Cross_Threshold;
+    void SetThresholdTo(float threshold_value) {
+      THRESHOLD_VALUE = threshold_value;
+    }
+    float GetThresholdValue() {
+      return THRESHOLD_VALUE;
+    }
+    bool DidCrossThreshold() {  //Came closer than the threshold value
+      float currentDistance = this->GetDistance();
+      if (currentDistance <= THRESHOLD_VALUE) {
+        return true;  //TRIGGERED
+      }
+      else {
+        return false; //NOT TRIGGERED
+      }
+    }
+
+    void InitiateTrigger() {
+      digitalWrite(TRIG_PIN, LOW);
+      delayMicroseconds(2);
+      digitalWrite(TRIG_PIN, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(TRIG_PIN, LOW);
+    }
+    float GetDuration() {
+      InitiateTrigger();
+      float Duration = pulseIn(ECHO_PIN, HIGH); //Get duration of the high pulse
+      return Duration;
+    }
+    float GetDistance() {
+      DISTANCE =  this->GetDuration() * 0.0343 / 2;
+      return DISTANCE;
+    }
+};
+
+#define ULTRASONIC_TX_PIN 2
+#define ULTRASONIC_RX_PIN 3
+
+#define ULTRASONIC1_THRESHOLD_VALUE_CM 20
+
+#define HAMMER_INACTIVE_TIME_MILLIS 3000
+#define HAMMER_ACTIVE_SMASH_TIME_MILLIS 550
+#define HAMMER_RETRIEVE_DELAY_PAUSE_TIME_MILLIS 1000
+
+#define ACTIVATE_RELAY_PIN 4  //COM - MOTOR, NC - GND, NO - 12V
+#define RETRIEVE_RELAY_PIN 5  //COM - MOTOR, NC - GND, NO - 12V
+
+#define LIMITER_SWITCH_PIN 6  //Use COM and NO
+
+Ultrasonic_Sensor Ultrasonic1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Ultrasonic1.AttachToTrigEcho(ULTRASONIC_TX_PIN, ULTRASONIC_RX_PIN);
+  Ultrasonic1.SetThresholdTo(ULTRASONIC1_THRESHOLD_VALUE_CM);
+
+  pinMode(ULTRASONIC_TX_PIN, OUTPUT);
+  pinMode(ULTRASONIC_RX_PIN, INPUT);
+  pinMode(ACTIVATE_RELAY_PIN, OUTPUT);
+  pinMode(RETRIEVE_RELAY_PIN, OUTPUT);
+  pinMode(LIMITER_SWITCH_PIN, INPUT);
+
+  Serial.begin(9600);
+  //Keep retrieving
+  Retrieve_Hammer();
+}
+
+void Retrieve_Hammer() {
+  Serial.println("RETRIVEING HAMMER");
+  while (digitalRead(LIMITER_SWITCH_PIN) == LOW) {
+    digitalWrite(RETRIEVE_RELAY_PIN, LOW);    //Energise Retrieving hammer
+    digitalWrite(ACTIVATE_RELAY_PIN, HIGH);
+  }
+  //Hold the hammer there
+  digitalWrite(RETRIEVE_RELAY_PIN, LOW);    //Energise Retrieving hammer
+  digitalWrite(ACTIVATE_RELAY_PIN, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float Distance = Ultrasonic1.GetDistance();
+  Serial.print("1 - ");
+  Serial.print(Distance);
+  Serial.print((Ultrasonic1.DidCrossThreshold()) ? " YES : " : " NO : ");
+  Serial.println(Ultrasonic1.GetThresholdValue());
+
+  if (Ultrasonic1.DidCrossThreshold()) {
+    Serial.println("Hitting with hammer");
+    digitalWrite(ACTIVATE_RELAY_PIN, LOW);  //SMASH
+    digitalWrite(RETRIEVE_RELAY_PIN, HIGH);
+    delay(HAMMER_ACTIVE_SMASH_TIME_MILLIS);
+
+    //Get the relays and the entire system to rest
+    digitalWrite(ACTIVATE_RELAY_PIN, LOW);  
+    digitalWrite(RETRIEVE_RELAY_PIN, LOW);
+
+    delay(HAMMER_RETRIEVE_DELAY_PAUSE_TIME_MILLIS);
+    //Retrieve hammer and don't do anything for a desired time
+    Retrieve_Hammer();
+    delay(HAMMER_INACTIVE_TIME_MILLIS);
+  }
+
+  if (digitalRead(LIMITER_SWITCH_PIN) != HIGH) {  
+    Retrieve_Hammer();
+  }
+  
+}
